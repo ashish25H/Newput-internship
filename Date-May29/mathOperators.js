c4e33618122936626b6593a077237b8b125b94b7
ashish25H <me.ashishkp@gmail.com>
2023-05-29 22:47:52 +0530

    Topic- JavaScript Fundamentals Code structure The modern mode, "use strict" Variables Data types Interaction: alert, prompt, confirm Type Conversions Basic operators, maths Comparisons Conditional branching: if, ? Logical operators

---
+++ Date-May29/mathOperators.js
@@ -0,0 +1,27 @@
+let a = 10;
+let b = 20;
+
+console.log(a-b);
+console.log(b-a);
+console.log(a*b);
+console.log(b/a);
+console.log(a/b);
+console.log(b%a);
+console.log(2**2); //2 power 2
+
+console.log(2 + 2 + '1');
+console.log('2' + 2 + 1);
+
+
+console.log(Number('\t \n'));
+console.log(Number('\n'));
+console.log(null === undefined);
+
+// alert( null > 0 );  // (1) false
+// alert( null == 0 ); // (2) false
+// alert( null >= 0 ); // (3) true
+
+
+// alert( undefined > 0 ); // false (1)
+// alert( undefined < 0 ); // false (2)
+// alert( undefined == 0 ); // false (3)
